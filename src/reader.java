8b4755f32fd1554aa9efeeaa9ecb40558c3224d2
vorbi <vorbi@DESKTOP-JFF0C9A>
2018-12-02 22:41:06 -0600

    update to frontend

--- src/reader.java
+++ src/reader.java
@@ -5,19 +5,26 @@ import java.util.Scanner;
 public class reader {
 	public static void main(String[] args) throws FileNotFoundException
     {
-        //Get scanner instance
+        int linesRead = 0; 
+		//Get scanner instance
         Scanner scanner = new Scanner(new File("E:/Import/MapleCo01GL_2008.csv"));
          
         //Set the delimiter used in file
         scanner.useDelimiter(",");
+        
+        LinkedList ll = new LinkedList();
          
         //Get all tokens and store them in some data structure
         //I am just printing them
         while (scanner.hasNext())
         {
-            System.out.print(scanner.next() + "|");
+            String line = scanner.nextLine();
+            ll.add(line);
+        	//System.out.print(line + "|");
+            linesRead ++;
         }
-         
+        //ll.print();
+        System.out.print("lines read in = " + linesRead+ " lines in linked list = " + ll.getSize());
         //Do not forget to close the scanner 
         scanner.close();
     }
